da552a0217bbf4705e0bd53d3f9bbfaa096ff24f
FFTampinongkol <fftampinongkol09@gmail.com>
2024-01-12 12:06:20 +0700

    up

---
+++ app/fe_LIPs.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def app():
+    st.title('Link Phishing Detection')
+    st.markdown(
+        """ In this case, we build the Machine Learning Model to detect malicious
+            links like phishing or legitimate. Details from the code and methods
+            can be read in my publication list on Google Scholar about
+            `Link Phishing Detection`.
+        """)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from multiapp import MultiApp
-from app import home, fe_LEDs
+from app import home, fe_LEDs, fe_LIPs
 
 st.set_page_config(page_title='| fftampinongkol', page_icon='👋')
 app = MultiApp()
 
 app.add_app("My Home Page", home.app)
 app.add_app("Leaf Diseases Detection", fe_LEDs.app)
+app.add_app("Link Phishing Detection", fe_LIPs.app)
 app.run()
