f8a154d226c0975f7d6657da807bbe29330a4cf3
FFTampinongkol <fftampinongkol09@gmail.com>
2025-10-08 14:39:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from app import home, fe_LEDs, try_code, fe_webScraping
+from app import home, fe_LEDs, try_code, fe_webScraping, fe_stockAnalytics
 
 st.set_page_config(page_title='| fftampinongkol', page_icon='👋')
 app = MultiApp()
@@ -9,4 +9,5 @@ app.add_app("My Home Page", home.app)
 app.add_app("Leaf Diseases Detection", fe_LEDs.app)
 app.add_app("Link Phishing Detection", try_code.app)
 app.add_app("Web Scraping", fe_webScraping.app)
+app.add_app("Stock Analytics", fe_stockAnalytics.app)
 app.run()
