7973f857f2cca9c61f67e22a042979ac05463ed2
FFTampinongkol <fftampinongkol09@gmail.com>
2025-10-08 16:56:58 +0700

    Update fe_stockAnalytics.py

--- app/fe_stockAnalytics.py
+++ app/fe_stockAnalytics.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import yfinance as yf
 
 def app():
-    st.title('Link Phishing Detection')
-    st.markdown(
-        """ In this case, we build the Machine Learning Model to detect malicious
-            links like phishing or legitimate. Details from the code and methods
-            can be read in my publication list on Google Scholar about
-            `Link Phishing Detection`.
+    st.title('Stock Analysis')
+    st.markdown(""" In this study, we develop a stock analysis model that utilizes the Support and Resistance approach to identify potential
+        entry and exit zones while confirming price movement strength through volume analysis. The methodology, code implementation, and
+        data processing details — including historical price retrieval, pivot-based level computation (S1–R2), volume confirmation, and
+        fair value estimation for buy/sell decisions — are thoroughly explained in my publication list and supporting materials available
+        on Google Scholar under the topic “Stock Analysis Using Support and Resistance.”.
         """)
+    cols = st.columns([1, 3])
